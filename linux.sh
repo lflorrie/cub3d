5c0dbbad4db5655129cc261422f5c3956b17c4b7
lflorrie <lflorrie@lflorrie.localdomain>
2021-04-30 23:53:22 +0300

    linux

--- linux.sh
+++ linux.sh
@@ -1,3 +1,8 @@
+git clone https://github.com/42Paris/minilibx-linux.git mlx_linux
+cd mlx_linux
+./configure
+cd ..
 mv Makefile Makefile_m && mv Makefile_l Makefile
 mv my_cub_utils.h my_cub_utils_m.h && mv my_cub_utils_l.h my_cub_utils.h
 make
+./cub3D tests_maps/test_valid_3.cub
